296136748517ce171f8ed35b6d62b30c41154826
mgj <MGJ@mgjs-MacBook-Pro.local>
2014-05-12 00:29:50 -0700

    v0.0.2

--- fixture.js
+++ fixture.js
@@ -6,9 +6,6 @@ sinon = require("sinon");
 
 Promise = require("es6-promise").Promise;
 
-
-/* istanbul ignore next */
-
 spy = function(stream) {
   var agent, fn;
   if (spy.free.length === 0) {
@@ -31,9 +28,6 @@ spy.free = [];
 
 spy.used = [];
 
-
-/* istanbul ignore next */
-
 extendCtx = function(fn) {
   this.thr = fn.factory(this.optA);
   this.thrX = fn.factory(this.optB);
@@ -48,8 +42,6 @@ extendCtx = function(fn) {
 
 bufferMode = {
   desc: 'streams in buffer mode:',
-
-  /* istanbul ignore next */
   before: function(fn) {
     return function() {
       var ctor;
@@ -74,8 +66,6 @@ bufferMode = {
       return this;
     };
   },
-
-  /* istanbul ignore next */
   after: function() {
     var agent, _i, _len, _ref, _results;
     _ref = spy.used;
@@ -90,8 +80,6 @@ bufferMode = {
 
 objectMode = {
   desc: 'streams in object mode:',
-
-  /* istanbul ignore next */
   before: function(fn) {
     return function() {
       var ctor;
@@ -116,8 +104,6 @@ objectMode = {
       return this;
     };
   },
-
-  /* istanbul ignore next */
   after: function() {
     var agent, _i, _len, _ref, _results;
     _ref = spy.used;
@@ -130,9 +116,6 @@ objectMode = {
   }
 };
 
-
-/* istanbul ignore next */
-
 Deferred = function() {
   this.promise = new Promise((function(_this) {
     return function(resolve, reject) {
@@ -143,29 +126,17 @@ Deferred = function() {
   return this;
 };
 
-
-/* istanbul ignore next */
-
 Deferred.prototype.resolve = function() {
   return this.resolve_.apply(this.promise, arguments);
 };
-
-
-/* istanbul ignore next */
 
 Deferred.prototype.reject = function() {
   return this.reject_.apply(this.promise, arguments);
 };
 
-
-/* istanbul ignore next */
-
 Deferred.prototype.then = function() {
   return this.promise.then.apply(this.promise, arguments);
 };
-
-
-/* istanbul ignore next */
 
 Deferred.prototype["catch"] = function() {
   return this.promise["catch"].apply(this.promise, arguments);
--- index.js
+++ index.js
@@ -5,10 +5,8 @@
   * @license MIT
   * @description 
   * # Through
-  * ##\# warning: this is not ready for consuption yet
-  * <!-- [![NPM][NPM1][NPM2]] -->
   *
-  * <!-- [![Build Status][1]][2] [![dependency status][3]][4] [![dev dependency status][5]][6] -->
+  * [![NPM version](https://badge.fury.io/js/super-stream.through.png)](https://npmjs.org/package/super-stream.through) [![Build Status](https://travis-ci.org/markuz-gj/super-stream.through.png?branch=master)](https://travis-ci.org/markuz-gj/super-stream.through) [![Dependency Status](https://david-dm.org/markuz-gj/super-stream.through.png)](https://david-dm.org/markuz-gj/super-stream.through) [![devDependency Status](https://david-dm.org/markuz-gj/super-stream.through/dev-status.png)](https://david-dm.org/markuz-gj/super-stream.through#info=devDependencies) [![Coverage Status](https://coveralls.io/repos/markuz-gj/super-stream.through/badge.png?branch=master)](https://coveralls.io/r/markuz-gj/super-stream.through?branch=master) [![MIT Licensed](http://img.shields.io/badge/license-MIT-blue.svg)](#license)
   * 
   * ##\# A basic wrapper function around [`through2@0.4.x`](https://github.com/rvagg/through2)
   *
@@ -17,9 +15,18 @@
   * Why shouldn't you use `through2` instead of this module?  
   * You wouldn't if all you need is a basic helper for creating [`stream.Transform`](http://nodejs.org/api/stream.html#stream_class_stream_transform).
   *
-  * But if you need some functional style transforms and other stream utilities and reduce your dependencies at the same time, **_this_** is your basic through stream you are looking for.
-  * 
-  * See also ...
+  * But if you need some functional style transforms and other stream utilities and reduce your dependencies at the same time, **_this_** is your basic through stream you are looking for. For all the API, go [here](http://markuz-gj.github.io/super-stream.through/)  
+  *
+  * See also.  
+  * [`super-stream`](https://github.com/markuz-gj/super-stream)  
+  * [`super-stream.each`](https://github.com/markuz-gj/super-stream.each)  
+  * [`super-stream.map`](https://github.com/markuz-gj/super-stream.map)  
+  * [`super-stream.reduce`](https://github.com/markuz-gj/super-stream.reduce)  
+  * [`super-stream.filter`](https://github.com/markuz-gj/super-stream.filter)  
+  * [`super-stream.junction`](https://github.com/markuz-gj/super-stream.junction)  
+  * [`super-stream.pipeline`](https://github.com/markuz-gj/super-stream.pipeline)  
+  *
+  * * * *
  */
 var buf, ctor, defaults, factory, isFunction, obj, through, through2;
 
@@ -39,11 +46,11 @@ defaults = require("lodash.defaults");
   * @return {transformStream} - A instance of `Transform` stream from `readable-stream@1.0.x`
   *
   * @desc 
-
-  * ##\## Basic usage:
+  * ##\## _through([options,] [transformFn,] [flushFn]);_
   *
   * ```javascript
   * var expect = require('chai').expect;
+  * var through = require("super-stream.through")
   *
   * var streamA = through.obj(function(counter, enc, done){
   *   counter += 1;
@@ -74,7 +81,8 @@ defaults = require("lodash.defaults");
   *   expect(chunk.toString()).to.be.equal('myData-myData');
   *   done();
   * });
-  *
+  *  
+  * streamA.pipe(streamB);
   * streamA.write(new Buffer('myData'));
   * ```
  */
@@ -104,19 +112,24 @@ through = function(cfg) {
   * @return {Transform} - A pre-configured `Transform` contructor from `readable-stream@1.0.x`
   *
   * @desc 
-  * ##\## _ through.ctor([options], [transformFn], [flushFn]); _
+  * ##\## _through.ctor([options,] [transformFn,] [flushFn]);_
   * Note: This is the same `ctor` method from `through2`  
   * If called without arguments, returns a passthrough `Transform` 
   *
   * ```javascript
+  * var Transform = require('readable-stream').Transform;
   * var Ctor = through.ctor({objectMode: true}, transformFn, flushFn);
-  * streamA = new Ctor();
-  *
+  * var streamA = new Ctor();
+  * 
   * // no need for the new operator
-  * streamB = Ctor(); 
+  * var streamB = Ctor(); 
   *
   * //overriding options
-  * streamC = Ctor({objectMode: false}); 
+  * var streamC = Ctor({objectMode: false}); 
+  *
+  * expect(streamA).to.be.an.instanceof(Transform);
+  * expect(streamB).to.be.an.instanceof(Transform);
+  * expect(streamC).to.be.an.instanceof(Transform);
   *
   * ```
  */
@@ -133,18 +146,18 @@ ctor = function(options, transform, flush) {
   * @return {transformStream} - A instance of `Transform` stream from `readable-stream@1.0.x`
   *
   * @desc 
-  * ##\## _ through.obj([transfromFn], [flushFn]) _
+  * ##\## _through.obj([transfromFn,] [flushFn])_
   * It is a conveniece method for `through({objectMode: true}, transformFn, flushFn);`  
   * If called without arguments, returns a passthrough `Transform` 
   *
   * Note: This is the same `obj` method from `through2`
   *
   * ```javascript
-  * var streamObj = thr.obj(function(string, enc, done){
+  * var stream = through.obj(function(string, enc, done){
   *   expect(string).to.be.deep.equal({data: 'myData'});
   *   done();
   * });
-  * streamObj.write({data: 'myData'});
+  * stream.write({data: 'myData'});
   * ```
  */
 
@@ -160,7 +173,7 @@ obj = function(transform, flush) {
   * @return {transformStream} - A instance of `Transform` stream from `readable-stream@1.0.x`
   *
   * @desc 
-  * ##\## _ through.buf([transfromFn], [flushFn]) _
+  * ##\## _through.buf([transfromFn,] [flushFn])_
   * It is a conveniece method for `through({objectMode: false}, transformFn, flushFn);`  
   * If called without arguments, returns a passthrough `Transform` 
   *
@@ -173,7 +186,7 @@ obj = function(transform, flush) {
   *   expect(chunk).to.be.equal(myData);
   *   expect(chunk).to.not.be.equal('my data');
   *   done();
-  * }):
+  * });
   * streamBuf.write(myData);
   * ```
  */
@@ -196,7 +209,7 @@ buf = function(transform, flush) {
   * @return {through} - A through function with `options` pre-configured as default
   *
   * @desc 
-  * ##\## _ through.factory([options]); _
+  * ##\## _through.factory([options]);_
   * A factory method for creating a custom `through` instance.  
   *
   * ```javascript
@@ -217,7 +230,7 @@ buf = function(transform, flush) {
   *   expect(chunk).to.be.equal(myData);
   *   expect(chunk).to.not.be.equal('my data');
   *   done();
-  * }):
+  * });
   * streamBuf.write(myData);
   * ```
  */
@@ -234,5 +247,27 @@ factory = function(cfg) {
   fn.buf = buf;
   return fn;
 };
+
+
+/**
+  * README.md's footer
+  * @desc
+  * License
+  * ---
+  *
+  * The MIT License (MIT)
+  *
+  * Copyright (c) 2014 Markuz GJ
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to
+  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  *
+  * [![NPM](https://nodei.co/npm/super-stream.through.png)](https://nodei.co/npm/super-stream.through/) [![NPM](https://nodei.co/npm-dl/super-stream.through.png)](https://nodei.co/npm/super-stream.through/)
+ */
 
 module.exports = factory();
